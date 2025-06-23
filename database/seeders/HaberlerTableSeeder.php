7a45d7486e193eeb8a8f7ca584ef81f2ec7fdee8
EmirGulKC <emir.gul@karecode.com.tr>
2025-06-23 21:41:46 +0300

    sahte haber için seeder

---
+++ database/seeders/HaberlerTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Haber; // haber modelimiz
+
+// sahte haber verilerini oluşturmak için Seeder sınıfı
+class HaberlerTableSeeder extends Seeder
+{
+
+    // run metodu, seeder çalıştırıldığında otomatik olarak çağrılır.
+    public function run(): void
+    {
+        // Haber modelimize factory kullanarak 250.000 adet sahte haber oluşturuyoruz.
+        Haber::factory()->count(250000)->create();
+
+        $this->command->info('250.000 adet haber başarıyla oluşturuldu!'); //bilgi mesajı
+    }
+}
